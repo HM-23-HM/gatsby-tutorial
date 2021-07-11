f9082d0fcbe15a22ede98d2f73117d42a7cd8630
Thor <73763814+HM-23-HM@users.noreply.github.com>
2021-07-11 07:14:45 -0500

    Gatsby Tutorial Part 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "",
+        trackingId: "G-438TMDNGJB",
       },
     },
     "gatsby-plugin-react-helmet",
